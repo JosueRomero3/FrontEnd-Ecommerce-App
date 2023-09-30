f8c57af632b650e09ef0d498d31eda65d528962a
Josue Romero Polo <128860111+JosueRomero3@users.noreply.github.com>
2023-09-30 08:36:34 -0500

    Update SearchInput.js

--- src/components/Form/SearchInput.js
+++ src/components/Form/SearchInput.js
@@ -10,7 +10,7 @@ const SearchInput = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        `https://ecommerce-app-api.onrender.com/api/v1/product/search/${values.keyword}`
+        `https://latiendita-7fxv.onrender.com/api/v1/product/search/${values.keyword}`
       );
       setValues({ ...values, results: data });
       navigate("/search");
