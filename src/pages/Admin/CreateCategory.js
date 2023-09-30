385b5a24b6dffa17fd25fb621506caae5bcd1d7b
Josue Romero Polo <128860111+JosueRomero3@users.noreply.github.com>
2023-09-30 08:39:51 -0500

    Update CreateCategory.js

--- src/pages/Admin/CreateCategory.js
+++ src/pages/Admin/CreateCategory.js
@@ -16,7 +16,7 @@ const CreateCategory = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("https://ecommerce-app-api.onrender.com/api/v1/category/create-category", {
+      const { data } = await axios.post("https://latiendita-7fxv.onrender.com/api/v1/category/create-category", {
         name,
       });
       if (data?.success) {
@@ -34,7 +34,7 @@ const CreateCategory = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("https://ecommerce-app-api.onrender.com/api/v1/category/get-category");
+      const { data } = await axios.get("https://latiendita-7fxv.onrender.com/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -53,7 +53,7 @@ const CreateCategory = () => {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        `https://ecommerce-app-api.onrender.com/api/v1/category/update-category/${selected._id}`,
+        `https://latiendita-7fxv.onrender.com/api/v1/category/update-category/${selected._id}`,
         { name: updatedName }
       );
       if (data?.success) {
@@ -73,7 +73,7 @@ const CreateCategory = () => {
   const handleDelete = async (pId) => {
     try {
       const { data } = await axios.delete(
-        `https://ecommerce-app-api.onrender.com/api/v1/category/delete-category/${pId}`
+        `https://latiendita-7fxv.onrender.com/api/v1/category/delete-category/${pId}`
       );
       if (data.success) {
         toast.success(`category is deleted`);
