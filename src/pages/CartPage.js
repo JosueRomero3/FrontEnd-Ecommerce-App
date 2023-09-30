b2df08d07fb4fd90b1143729a64c61b320e8d070
Josue Romero Polo <128860111+JosueRomero3@users.noreply.github.com>
2023-09-30 10:44:05 -0500

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -102,7 +102,7 @@ const CartPage = () => {
                 <div className="row card flex-row w-75 m-3" key={p._id}>
                   <div className="col-md-4">
                     <img
-                      src={`/api/v1/product/product-photo/${p._id}`}
+                      src={`https://latiendita-7fxv.onrender.com/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="100%"
