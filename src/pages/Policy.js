21e3d1474ee9d82b8e18963885d88fd70c2b177a
Josue Romero Polo <128860111+JosueRomero3@users.noreply.github.com>
2023-10-01 21:07:30 -0500

    Update Policy.js

--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -11,7 +11,7 @@ const Policy = () => {
           <h6>TRATAMIENTO DE DATOS PERSONALES</h6>
           <p>
 
-            Debido a la actividad comercial que desarrolla CONTINENTE S.A.S con NIT. 890.101.279-0 y dando cumplimiento al Ley 1581 de 2012, les informamos a los titulares de los datos personales que lleva a cabo los siguientes tipos de Tratamiento sobre sus datos personales. Las clases de Tratamiento que se enuncian a continuación deben ser autorizadas por los titulares de los datos de manera previa, expresa e informada:</p>
+            Debido a la actividad comercial que desarrolla CONTINENTE S.A.S con NIT. 3.910.114.475-0 y dando cumplimiento al Ley 1581 de 2012, les informamos a los titulares de los datos personales que lleva a cabo los siguientes tipos de Tratamiento sobre sus datos personales. Las clases de Tratamiento que se enuncian a continuación deben ser autorizadas por los titulares de los datos de manera previa, expresa e informada:</p>
             <p>Utilizar la información proporcionada por los usuarios y titulares de los datos personales para las finalidades consignadas en esta Política en la Política de privacidad de datos.
 
 Transferir a terceros los datos personales para efectos de mercadeo y las otras finalidades que se describen en la Política de privacidad de datos.</p>
