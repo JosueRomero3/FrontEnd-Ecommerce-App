b67e87ab1fce51b42f988c5d512d40e2be5d6907
Josue Romero Polo <128860111+JosueRomero3@users.noreply.github.com>
2023-10-10 20:37:32 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,7 +16,7 @@ const About = () => {
           <h1 className="text-center"> Nosotros</h1>
           <p className="text-justify mt-2">
          
-¡Bienvenido a www.paninitienda.com tu principal portal de internet para compras de artículos LaTiendita®! Ofrecemos la más amplia selección y variedad a los mejores precios en la puerta de tu casa. Es una tienda online de fácil y rápido acceso, está diseñada para hacer de tu compra la mejor experiencia. Visítanos con frecuencia para que no te pierdas las diferentes ofertas y los nuevos productos que estamos continuamente lanzando.
+¡Bienvenido a www.laTiendita.com tu principal portal de internet para compras de artículos LaTiendita®! Ofrecemos la más amplia selección y variedad a los mejores precios en la puerta de tu casa. Es una tienda online de fácil y rápido acceso, está diseñada para hacer de tu compra la mejor experiencia. Visítanos con frecuencia para que no te pierdas las diferentes ofertas y los nuevos productos que estamos continuamente lanzando.
           </p>
         </div>
       </div>
